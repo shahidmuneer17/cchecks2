f39ce01892de5f33c5f5252dc63e40e6e7d7f8e8
syncomuae <120463069+syncomuae@users.noreply.github.com>
2024-02-08 14:51:48 +0400

    nn

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'user_name' => $data['name'],
+            'user_name' => $data['user_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
